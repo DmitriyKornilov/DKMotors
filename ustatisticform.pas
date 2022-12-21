104f492b93f89a9b3cf07f7faeb250dbff3a3813
Dmitriy Kornilov <dakor2017@yandex.ru>
2022-12-21 11:30:56 +0300

    minor fix

--- ustatisticform.pas
+++ ustatisticform.pas
@@ -6,34 +6,25 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Buttons,
-  StdCtrls, rxctrls, DateTimePicker, DividerBevel, USQLite, DK_Vector,
-  DK_DateUtils;
+  StdCtrls, VirtualTrees, rxctrls, DateTimePicker, DividerBevel, USQLite,
+  DK_Vector, DK_DateUtils, DK_VSTTables, SheetUtils;
 
 type
 
   { TStatisticForm }
 
   TStatisticForm = class(TForm)
-    DateTimePicker1: TDateTimePicker;
-    DateTimePicker2: TDateTimePicker;
-    DividerBevel1: TDividerBevel;
-    DividerBevel2: TDividerBevel;
-    DividerBevel3: TDividerBevel;
-    ExportButton: TRxSpeedButton;
-    Label1: TLabel;
-    NumberListCheckBox: TCheckBox;
-    NumberListPanel: TPanel;
-    OrderNumCheckBox: TCheckBox;
+    Panel1: TPanel;
     Panel2: TPanel;
-    RadioButton1: TRadioButton;
-    RadioButton2: TRadioButton;
-    RadioButton3: TRadioButton;
-    RadioButton4: TRadioButton;
-    ReportPeriodPanel: TPanel;
-    ToolPanel: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    Splitter1: TSplitter;
+    VT1: TVirtualStringTree;
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
-
+    StatisticList: TVSTTable;
+    procedure SetStatisticList;
   public
     procedure ShowStatistic;
   end;
@@ -52,8 +43,34 @@ uses UMainForm;
 procedure TStatisticForm.FormCreate(Sender: TObject);
 begin
   MainForm.SetNamesPanelsVisible(True, False);
-  DateTimePicker1.Date:= Date;
-  DateTimePicker2.Date:= FirstDayInMonth(Date);
+  StatisticList:= TVSTTable.Create(VT1);
+  SetStatisticList;
+  //DateTimePicker1.Date:= Date;
+  //DateTimePicker2.Date:= FirstDayInMonth(Date);
+end;
+
+procedure TStatisticForm.FormDestroy(Sender: TObject);
+begin
+  if Assigned(StatisticList) then FreeAndNil(StatisticList);
+end;
+
+procedure TStatisticForm.SetStatisticList;
+var
+  V: TStrVector;
+begin
+  V:= VCreateStr([
+    'Статистика 1',
+    'Статистика 2'
+  ]);
+
+  StatisticList.SelectedBGColor:= COLOR_BACKGROUND_SELECTED;
+  StatisticList.HeaderVisible:= False;
+  StatisticList.CanSelect:= True;
+  StatisticList.CanUnselect:= False;
+  StatisticList.AddColumn('Список');
+  StatisticList.SetColumn('Список', V, taLeftJustify);
+  StatisticList.Draw;
+  StatisticList.Select(0);
 end;
 
 procedure TStatisticForm.ShowStatistic;
